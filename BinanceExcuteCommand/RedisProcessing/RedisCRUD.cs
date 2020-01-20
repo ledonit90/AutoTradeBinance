f9ba8012db97600eddcac27bd55b808ab1630509
Le Van Don <don.lv@citigo.com.vn>
2020-01-20 19:31:01 +0700

    update REdis CRUD

--- BinanceExcuteCommand/RedisProcessing/RedisCRUD.cs
+++ BinanceExcuteCommand/RedisProcessing/RedisCRUD.cs
@@ -1,12 +1,32 @@
-﻿using RemitanoPrices.Models;
-
+﻿using Newtonsoft.Json;
+using RemitanoPrices.Models;
+using StackExchange.Redis;
+using System.Threading.Tasks;
 namespace RedisProcessing
 {
     public class RedisCRUD
     {
-        public string savePrice(CoinPrice price, int unixTime)
+        private IDatabaseAsync db; 
+        public RedisCRUD()
         {
+            db = RedisConnectorHelper.RedisCache(3);
+        }
+        public async Task savePriceAsync(CoinPrice price, int unixTime)
+        {
+            string content = JsonConvert.SerializeObject(price);
+            string key = "price_" + price.CodeCoin;
+            await db.HashSetAsync(key, new HashEntry[] { new HashEntry(unixTime, content) });
+        }
 
+        public async Task RecylebinPriceAsync(string key,int beforeUnixTime, int distance, int space)
+        {
+            if (distance == 0 || space == 0 || beforeUnixTime == 0 || string.IsNullOrWhiteSpace(key)) return;
+            int Round = distance / space;
+            for(int i = 1; i <= Round; i++)
+            {
+                int name = beforeUnixTime + i * space;
+                await db.HashDeleteAsync(key, name);
+            }
         }
     }
 }
