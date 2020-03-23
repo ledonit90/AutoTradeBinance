616f5f37b2ecf146beb080779bf3f8e0f48fc673
Le Van Don <don.lv@citigo.com.vn>
2020-03-23 13:32:15 +0700

    update HttpRequestHelper

--- BinanceExcuteCommand/Remibit.Utility/Helper/HttpRequestHelper.cs
+++ BinanceExcuteCommand/Remibit.Utility/Helper/HttpRequestHelper.cs
@@ -8,10 +8,18 @@ namespace Remibit.Utility.Helper
     {
         public string url { get; set; }
         public  Dictionary<string, string> lsHeader { get; set; }
+        public  Dictionary<string, string> dic_querystring { get; set; }
+        public  object obj_querystring { get; set; }
+        public  string str_querystring { get; set; }
         public HttpClient iweb { get; set; }
 
+        public HttpClient Builder()
+        {
+            AddHeader();
+            return iweb;
+        }
 
-        public void AddHeader()
+        private void AddHeader()
         {
             if(lsHeader != null && lsHeader.Count()!=0)
             {
@@ -20,6 +28,20 @@ namespace Remibit.Utility.Helper
                     iweb.DefaultRequestHeaders.Add(t.Key, t.Value);
                 }
             }
+        }
+        private void UrlBuilder()
+        {
+            if(!string.IsNullOrEmpty(str_querystring))
+            {
+                url += str_querystring;
+            }else if(obj_querystring != null)
+            {
+                url += ParameterHelper.ObjectToQueryString(obj_querystring);
+            } else if(dic_querystring != null && dic_querystring.Count() != 0)
+            {
+                url += ParameterHelper.ObjectToQueryString(obj_querystring);
+            }
+
         }
     }
 }
